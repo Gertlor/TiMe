f4b9f6eb8f4244e1f340640549a2488da1df5cf8
Gerrit Louis <gerritlouis@gmail.com>
2019-12-12 19:52:11 +0100

    Initial Commit

---
+++ TiMe WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  TiMe WatchKit Extension
+//
+//  Created by Gerrit Louis on 12/12/2019.
+//  Copyright © 2019 Gerrit Louis de Beuze. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
