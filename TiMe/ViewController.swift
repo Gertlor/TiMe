b2fb6106fa6a93ed17abc0d15888ffff11d0a7ec
Gerrit Louis <gerritlouis@gmail.com>
2020-01-01 14:59:35 +0100

    Add mainscreen, adds timer label, timer button and tableview for projects.

--- TiMe/ViewController.swift
+++ TiMe/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+	@IBOutlet weak var timerLabel: UILabel!
+	@IBOutlet weak var projectTableView: UITableView!
+	@IBOutlet weak var startStopButton: UIButton!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
-
-
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 3;
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = UITableViewCell(style: UITableViewCell.CellStyle.value1, reuseIdentifier: "Cell")
+		cell.backgroundColor = self.view.backgroundColor;
+		cell.textLabel?.text = "Project";
+		cell.detailTextLabel?.text = "00:00:00";
+		
+		return cell;
+	}
+	
+	@IBAction func startStopButtonPressed(_ sender: UIButton) {
+		
+		
+	}
 }
 
