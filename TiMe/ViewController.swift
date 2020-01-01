0b569df8e6a97d9ac8cb361e14e64eae18e6e39a
Gerrit Louis <gerritlouis@gmail.com>
2020-01-01 16:08:08 +0100

    Adds a working timer

--- TiMe/ViewController.swift
+++ TiMe/ViewController.swift
@@ -10,31 +10,71 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+	var timer = Timer()
+	var hours: Int = 0
+	var minutes: Int = 0
+	var seconds: Int = 0
+	var timerString: String = ""
+	
+	var startTimer: Bool = true
+	var addProject: Bool = false
+	
 	@IBOutlet weak var timerLabel: UILabel!
 	@IBOutlet weak var projectTableView: UITableView!
 	@IBOutlet weak var startStopButton: UIButton!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		timerLabel.text = "00:00:00"
 	}
 	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return 3;
+		return 3
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = UITableViewCell(style: UITableViewCell.CellStyle.value1, reuseIdentifier: "Cell")
-		cell.backgroundColor = self.view.backgroundColor;
-		cell.textLabel?.text = "Project";
-		cell.detailTextLabel?.text = "00:00:00";
+		cell.backgroundColor = self.view.backgroundColor
+		cell.textLabel?.text = "Project"
+		cell.detailTextLabel?.text = "00:00:00"
 		
-		return cell;
+		return cell
 	}
 	
-	@IBAction func startStopButtonPressed(_ sender: UIButton) {
+	@IBAction func startStop(_ sender: UIButton) {
+		if startTimer == true {
+			timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+			startTimer = false
+			startStopButton.setImage(UIImage(named: "stop.png"), for: UIControl.State.normal)
+			addProject = true
+		} else {
+			timer.invalidate()
+			startTimer = true
+			startStopButton.setImage(UIImage(named:"start.png"), for: UIControl.State.normal)
+			addProject = false;
+			timerLabel.text = "00:00:00"
+		}
+	}
+	
+	@objc func updateTimer() {
+		seconds += 1
+		if seconds == 60 {
+			minutes += 1
+			seconds = 0
+		}
 		
+		if minutes == 60 {
+			hours += 1
+			minutes = 0
+		}
 		
+		let secondsString = seconds > 9 ? "\(seconds)" : "0\(seconds)"
+		let minutesString = minutes > 9 ? "\(minutes)" : "0\(minutes)"
+		let hoursString = hours > 9 ? "\(hours)" : "0\(hours)"
+		
+		timerString = "\(hoursString):\(minutesString):\(secondsString)"
+		print(timerString)
+		timerLabel.text = timerString
 	}
 }
 
