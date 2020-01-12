9fb567b52cde1883d550b31a276185ad13afba2d
Gerrit Louis <gerritlouis@gmail.com>
2020-01-12 16:29:06 +0100

    adds the request for  retrieven the entries of the last 7 days

--- TiMe/Controller/CustomDateViewController.swift
+++ TiMe/Controller/CustomDateViewController.swift
@@ -10,27 +10,80 @@ import UIKit
 import CoreData
 
 class CustomDateViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+	
+	var entries = [Entry]()
+	
+	let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+	
 	@IBOutlet weak var selectDateButton: UIButton!
 	@IBOutlet weak var selectDatePicker: UIDatePicker!
 	@IBOutlet weak var entriesTableView: UITableView!
 	
 	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return 1;
+		return entries.count
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "DateEntryCell", for: indexPath) as! CustomDateEntryTableViewCell
 		
+		let project = entries[indexPath.row].parentProject?.name
+		let entryName = entries[indexPath.row].timeDescription
+		
+		cell.entryNameLabel?.text = "\(entryName ?? "No Name") - \(project ?? "No Project")"
+		cell.durationLabel?.text = entries[indexPath.row].timeStamp
+		
+		let dateFormatter = DateFormatter()
+		dateFormatter.locale = Locale(identifier: "nl_NL")
+		dateFormatter.dateFormat = "EEE, d MMM yyyy - HH:mm"
+		
+		cell.dateLabel?.text = dateFormatter.string(from: entries[indexPath.row].startTime ?? Date())
+		
 		return cell
 		
+	}
+	
+	func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+		if editingStyle == .delete {
+			context.delete(entries[indexPath.row])
+			entries.remove(at: indexPath.row)
+			saveContext()
+		}
 	}
 	
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+		loadEntries()
     }
+	
+	func loadEntries(request: NSFetchRequest<Entry> = Entry.fetchRequest(), predicate: NSPredicate? = nil) {
+		
+		let dateFromSevenDaysAgo = Date().addingTimeInterval(-604800) //-604800
+		
+		let predicate = NSPredicate(format: "startTime >= %@", argumentArray: [dateFromSevenDaysAgo])
+		
+		request.predicate = predicate
+		
+		do {
+			entries = try context.fetch(request)
+			entries.reverse()
+		} catch {
+			print("Error fetching data from context \(error)")
+		}
+		
+		entriesTableView.reloadData()
+	}
+	
+	func saveContext() {
+		do {
+			try context.save()
+		} catch {
+			print("Error saving context \(error)")
+		}
+		
+		loadEntries()
+	}
 	
 	
 	@IBAction func showHideDatePicker(_ sender: UIButton) {
